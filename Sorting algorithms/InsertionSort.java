0006a10bac6cd1a13bd2d5ab4032eaa78015ceb4
Ikbal Rasimov <44362017+Ikbal01@users.noreply.github.com>
2019-04-07 15:02:12 +0300

    Update InsertionSort.java

--- Sorting algorithms/InsertionSort.java
+++ Sorting algorithms/InsertionSort.java
@@ -1,18 +1,16 @@
 public class InsertionSort {
 
-    public static void insertionSort(int[] arr) {
-        int temp;
-
+   private static void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            for(int j = i ; j > 0 ; j--) {
-                if(arr[j] < arr[j-1]){
-                    temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                } else {
-                    break;
-                }
+            int currNumber = arr[i];
+            int j = i;
+
+            while (j > 0 && arr[j - 1] > currNumber) {
+                arr[j] = arr[j - 1];
+                j--;
             }
+
+            arr[j] = currNumber;
         }
     }
 }
